8f113db859cab44f04136424b1186b9e6796ecaa
Arnab Saha <saharnab1641@gmail.com>
2021-06-01 22:27:57 +0530

    Added profile picture upload

--- src/api/components/auth/routes.ts
+++ src/api/components/auth/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { FileTransferService } from "../../../services/file-transfer";
 
 import { IComponentRoutes } from "../index";
 import { AuthController } from "./controller";
@@ -6,13 +7,20 @@ import { AuthController } from "./controller";
 export class AuthRoutes implements IComponentRoutes<AuthController> {
   readonly controller: AuthController = new AuthController();
   readonly router: Router = Router();
+  private fileTransferService: FileTransferService;
 
   public constructor() {
+    this.fileTransferService = new FileTransferService();
     this.initRoutes();
   }
 
   initRoutes(): void {
     this.router.post("/signup", this.controller.doLocalRegister);
     this.router.post("/login", this.controller.doLocalLogin);
+    this.router.post(
+      "/updatepicture",
+      this.fileTransferService.multer.single("file"),
+      this.controller.updatePicture
+    );
   }
 }
